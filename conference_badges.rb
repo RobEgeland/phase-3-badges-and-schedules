99fc05987b6fa8140172b9b01f8b8e2aadd51598
Robert Egeland <robegeland@yahoo.com>
2022-05-04 20:02:16 -0500

    commit tree 700190c1ccc6dd870179e036f3083b813090031c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+require "pry"
+
+def badge_maker(name) 
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badge_arr = []
+    array.map {|arr| badge_arr << "Hello, my name is #{arr}."}
+    badge_arr
+end
+
+def assign_rooms(arr)
+    room_array = []
+    arr.each_with_index do |speaker, index|
+        room_array << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    end
+    room_array
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each do |badge|
+        puts badge
+    end
+    assign_rooms(arr).each do |badge|
+        puts badge
+    end
+end
